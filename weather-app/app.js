0767e26e1c208703b23e4370eb05696b861e0076
Marcio Woitek <woitek@usp.br>
2021-07-07 19:03:57 -0300

    32 - Customizing HTTP Requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,9 +4,11 @@ const request = require("postman-request");
 const getAPIAccessKey = () => fs.readFileSync("weatherstack").toString().trim();
 const latitude = "-25.50";
 const longitude = "-49.29";
-const url = `http://api.weatherstack.com/current?access_key=${getAPIAccessKey()}&query=${latitude},${longitude}`;
+const unitsParameter = "units=f";
+const url = `http://api.weatherstack.com/current?access_key=${getAPIAccessKey()}&query=${latitude},${longitude}&${unitsParameter}`;
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.current);
+request({ url: url, json: true }, (error, response) => {
+    console.log(
+        `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`
+    );
 });
