42cbfb6f5ced517241b16f3061e90aa2efabab8d
Marcio Woitek <woitek@usp.br>
2021-07-07 20:53:48 -0300

    33 - An HTTP Request Challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,14 +1,41 @@
 const fs = require("fs");
 const request = require("postman-request");
 
-const getAPIAccessKey = () => fs.readFileSync("weatherstack").toString().trim();
+const getAPIAccessKey = (fileName) =>
+    fs.readFileSync(fileName).toString().trim();
+
+const weatherstackKey = getAPIAccessKey("weatherstack");
 const latitude = "-25.50";
 const longitude = "-49.29";
 const unitsParameter = "units=f";
-const url = `http://api.weatherstack.com/current?access_key=${getAPIAccessKey()}&query=${latitude},${longitude}&${unitsParameter}`;
+const weatherstackUrl =
+    "http://api.weatherstack.com/current?access_key=" +
+    weatherstackKey +
+    "&query=" +
+    latitude +
+    "," +
+    longitude +
+    "&" +
+    unitsParameter;
 
-request({ url: url, json: true }, (error, response) => {
+request({ url: weatherstackUrl, json: true }, (error, response) => {
     console.log(
-        `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`
+        response.body.current.weather_descriptions[0] +
+            ". It is currently " +
+            response.body.current.temperature +
+            " degrees out. It feels like " +
+            response.body.current.feelslike +
+            " degrees out."
     );
+});
+
+const mapboxKey = getAPIAccessKey("mapbox");
+const mapboxUrl =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=" +
+    mapboxKey +
+    "&limit=1";
+
+request({ url: mapboxUrl, json: true }, (error, response) => {
+    console.log(`Latitude: ${response.body.features[0].center[1]}`);
+    console.log(`Longitude: ${response.body.features[0].center[0]}`);
 });
