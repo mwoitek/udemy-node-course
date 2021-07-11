c5ef72c483e5ea53e7b01bdfab2c366914e24f0d
Marcio Woitek <woitek@usp.br>
2021-07-11 19:19:18 -0300

    Lecture 47: Dynamic Pages with Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,10 +1,34 @@
+/** @format */
+
 const path = require("path");
 const express = require("express");
 
 const app = express();
 
+app.set("view engine", "hbs");
+
 const publicDirectoryPath = path.join(__dirname, "../public");
 app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+    res.render("index", {
+        title: "Weather",
+        name: "Marcio Woitek",
+    });
+});
+
+app.get("/about", (req, res) => {
+    res.render("about", {
+        title: "About Me",
+        name: "Marcio Woitek",
+    });
+});
+
+app.get("/help", (req, res) => {
+    res.render("help", {
+        helpText: "This is some helpful text!",
+    });
+});
 
 app.get("/weather", (req, res) => {
     res.send({
