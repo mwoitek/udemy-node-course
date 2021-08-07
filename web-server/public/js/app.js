ba1a244693aabfd70fbc99e4555f77ba235c1cb0
Marcio Woitek <woitek@usp.br>
2021-08-07 19:36:27 -0300

    Lecture 58: Creating a Search Form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,9 +1,17 @@
 console.log('Client side JavaScript file is loaded!');
 
-fetch('http://localhost:3000/weather?address=curitiba')
-  .then((response) => response.json())
-  .then((data) => {
-    if (data.error) return console.error(`Error: ${data.error}`);
-    console.log(`Location: ${data.location}`);
-    console.log(`Forecast: ${data.forecast}`);
-  });
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const address = search.value;
+  fetch(`http://localhost:3000/weather?address=${address}`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.error) return console.error(`Error: ${data.error}`);
+      console.log(`Location: ${data.location}`);
+      console.log(`Forecast: ${data.forecast}`);
+    });
+});
