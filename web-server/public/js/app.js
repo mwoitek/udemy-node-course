9be909bbe40e3c7eed9943e395efa1c218ad7f25
Marcio Woitek <woitek@usp.br>
2021-08-07 20:11:26 -0300

    Lecture 59: Wiring up the User Interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,17 +1,25 @@
 console.log('Client side JavaScript file is loaded!');
 
-const weatherForm = document.querySelector('form');
+const message1 = document.querySelector('#message-1');
+const message2 = document.querySelector('#message-2');
 const search = document.querySelector('input');
+const weatherForm = document.querySelector('form');
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
+
+  message1.textContent = 'Loading...';
+  message2.textContent = '';
 
   const address = search.value;
   fetch(`http://localhost:3000/weather?address=${address}`)
     .then((response) => response.json())
     .then((data) => {
-      if (data.error) return console.error(`Error: ${data.error}`);
-      console.log(`Location: ${data.location}`);
-      console.log(`Forecast: ${data.forecast}`);
+      if (data.error) {
+        message1.textContent = `Error: ${data.error}`;
+      } else {
+        message1.textContent = `Location: ${data.location}`;
+        message2.textContent = `Forecast: ${data.forecast}`;
+      }
     });
 });
