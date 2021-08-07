efb60a19ccb8660d10613ad2a528bb5c1c0f58e0
Marcio Woitek <woitek@usp.br>
2021-08-07 18:56:26 -0300

    Lecture 57: Browser HTTP Requests with fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,9 @@
 console.log('Client side JavaScript file is loaded!');
+
+fetch('http://localhost:3000/weather?address=curitiba')
+  .then((response) => response.json())
+  .then((data) => {
+    if (data.error) return console.error(`Error: ${data.error}`);
+    console.log(`Location: ${data.location}`);
+    console.log(`Forecast: ${data.forecast}`);
+  });
